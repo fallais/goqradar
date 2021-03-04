de95568f735bf0eedac017dda74fd7a3c89bdd1b
Michael FAURE <michael.faure@sogeti.com>
2021-03-04 14:27:41 +0100

    add functions in SIEM: UpdateOffense & CreateoffenseNote

--- siem.go
+++ siem.go
@@ -23,56 +23,66 @@ type OffenseType struct {
 	PropertyName string `json:"property_name"`
 }
 
-// Rule is a QRadar rule.
-type Rule struct {
+// Offense is QRadar offense
+type Offense struct {
+	AssignedTo                 string       `json:"assigned_to"`
+	Categories                 []string     `json:"categories"`
+	CategoryCount              int          `json:"category_count"`
+	CloseTime                  int          `json:"close_time"`
+	ClosingReasonID            int          `json:"closing_reason_id"`
+	ClosingUser                string       `json:"closing_user"`
+	Credibility                int          `json:"credibility"`
+	Description                string       `json:"description"`
+	DestinationNetworks        []string     `json:"destination_networks"`
+	DeviceCount                int          `json:"device_count"`
+	DomainID                   int          `json:"domain_id"`
+	EventCount                 int          `json:"event_count"`
+	FirstPersistedTime         int          `json:"first_persisted_time"`
+	FlowCount                  int          `json:"flow_count"`
+	FollowUp                   bool         `json:"follow_up"`
+	ID                         int          `json:"id"`
+	Inactive                   bool         `json:"inactive"`
+	LastPersistedTime          int          `json:"last_persisted_time"`
+	LastUpdatedTime            int          `json:"last_updated_time"`
+	LocalDestinationAddressIds []int        `json:"local_destination_address_ids"`
+	LocalDestinationCount      int          `json:"local_destination_count"`
+	LogSources                 []LogSources `json:"log_sources"`
+	Magnitude                  int          `json:"magnitude"`
+	OffenseSource              string       `json:"offense_source"`
+	OffenseType                int          `json:"offense_type"`
+	PolicyCategoryCount        int          `json:"policy_category_count"`
+	Protected                  bool         `json:"protected"`
+	Relevance                  int          `json:"relevance"`
+	RemoteDestinationCount     int          `json:"remote_destination_count"`
+	Rules                      []Rules      `json:"rules"`
+	SecurityCategoryCount      int          `json:"security_category_count"`
+	Severity                   int          `json:"severity"`
+	SourceAddressIds           []int        `json:"source_address_ids"`
+	SourceCount                int          `json:"source_count"`
+	SourceNetwork              string       `json:"source_network"`
+	StartTime                  int          `json:"start_time"`
+	Status                     string       `json:"status"`
+	UsernameCount              int          `json:"username_count"`
+}
+
+// LogSources is a QRadar log source
+type LogSources struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	TypeID   int    `json:"type_id"`
+	TypeName string `json:"type_name"`
+}
+
+// Rules is a QRadar rules
+type Rules struct {
 	ID   int    `json:"id"`
 	Type string `json:"type"`
 }
 
-// Offense is a QRadar offense.
-type Offense struct {
-	UsernameCount              int      `json:"username_count"`
-	Description                string   `json:"description"`
-	Rules                      []Rule   `json:"rules"`
-	EventCount                 int      `json:"event_count"`
-	FlowCount                  int      `json:"flow_count"`
-	AssignedTo                 string   `json:"assigned_to"`
-	SecurityCategoryCount      int      `json:"security_category_count"`
-	FollowUp                   bool     `json:"follow_up"`
-	SourceAddressIds           []int    `json:"source_address_ids"`
-	SourceCount                int      `json:"source_count"`
-	Inactive                   bool     `json:"inactive"`
-	Protected                  bool     `json:"protected"`
-	CategoryCount              int      `json:"category_count"`
-	SourceNetwork              string   `json:"source_network"`
-	DestinationNetworks        []string `json:"destination_networks"`
-	ClosingUser                string   `json:"closing_user"`
-	CloseTime                  int64    `json:"close_time"`
-	RemoteDestinationCount     int      `json:"remote_destination_count"`
-	StartTime                  int64    `json:"start_time"`
-	LastUpdatedTime            int64    `json:"last_updated_time"`
-	Credibility                int      `json:"credibility"`
-	Magnitude                  int      `json:"magnitude"`
-	ID                         int      `json:"id"`
-	Categories                 []string `json:"categories"`
-	Severity                   int      `json:"severity"`
-	PolicyCategoryCount        int      `json:"policy_category_count"`
-	DeviceCount                int      `json:"device_count"`
-	ClosingReasonID            int      `json:"closing_reason_id"`
-	OffenseType                int      `json:"offense_type"`
-	Relevance                  int      `json:"relevance"`
-	DomainID                   int      `json:"domain_id"`
-	OffenseSource              string   `json:"offense_source"`
-	LocalDestinationAddressIds []int    `json:"local_destination_address_ids"`
-	LocalDestinationCount      int      `json:"local_destination_count"`
-	Status                     string   `json:"status"`
-	Notes                      []*Note  `json:"notes,omitempty"`
-}
-
 // Note is a QRadar note
 type Note struct {
-	ID         int    `json:"id"`
 	CreateTime int    `json:"create_time"`
+	ID         int    `json:"id"`
 	NoteText   string `json:"note_text"`
 	Username   string `json:"username"`
 }
@@ -232,8 +242,48 @@ func (endpoint *Endpoint) GetOffense(ctx context.Context, id int, fields string)
 }
 
 // UpdateOffense with given ID.
-func (endpoint *Endpoint) UpdateOffense(ctx context.Context, id int, assignedTo string) error {
-	return nil
+func (endpoint *Endpoint) UpdateOffense(ctx context.Context, id, closingReasonID int, assignedTo, fields, status string, followUp, protected bool) (*Offense, error) {
+	// Options
+	options := []Option{}
+	if fields != "" {
+		options = append(options, WithParam("closingReasonID", strconv.Itoa(closingReasonID)))
+	}
+	if fields != "" {
+		options = append(options, WithParam("assignedTo", assignedTo))
+	}
+	if fields != "" {
+		options = append(options, WithParam("fields", fields))
+	}
+	if fields != "" {
+		options = append(options, WithParam("status", status))
+	}
+	if fields != "" {
+		options = append(options, WithParam("followUp", strconv.FormatBool(followUp)))
+	}
+	if fields != "" {
+		options = append(options, WithParam("protected", strconv.FormatBool(protected)))
+	}
+
+	// Do the request
+	resp, err := endpoint.client.do(ctx, http.MethodGet, "/siem/offenses/"+strconv.Itoa(id), options...)
+	if err != nil {
+		return nil, fmt.Errorf("error while calling the endpoint: %s", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error with the status code: %d", resp.StatusCode)
+	}
+
+	// Prepare the response
+	var response *Offense
+
+	// Decode the response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("error while decoding the response: %s", err)
+	}
+
+	return response, nil
 }
 
 // ListOffenseNotes returns the notes of the given offense.
@@ -264,8 +314,33 @@ func (endpoint *Endpoint) ListOffenseNotes(ctx context.Context, id string) ([]*N
 }
 
 // CreateOffenseNote ...
-func (endpoint *Endpoint) CreateOffenseNote(ctx context.Context, id string) ([]*Note, int, error) {
-	return nil, 0, nil
+func (endpoint *Endpoint) CreateOffenseNote(ctx context.Context, id int, noteText, fields string) (*Note, error) {
+	// Options
+	options := []Option{}
+	if fields != "" {
+		options = append(options, WithParam("fields", fields))
+	}
+
+	// Do the request
+	resp, err := endpoint.client.do(ctx, http.MethodGet, "/siem/offenses/"+strconv.Itoa(id)+"/notes"+noteText, options...)
+	if err != nil {
+		return nil, fmt.Errorf("error while calling the endpoint: %s", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error with the status code: %d", resp.StatusCode)
+	}
+
+	// Prepare the response
+	var response *Note
+
+	// Decode the response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("error while decoding the response: %s", err)
+	}
+
+	return response, nil
 }
 
 //------------------------------------------------------------------------------
